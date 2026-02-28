497d68600d4b080342ccb1b1f30672bcfc10bc34
JFSAINTS <65475709+JFSAINTS@users.noreply.github.com>
2026-02-28 15:58:55 +0000

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.d5f4e1fd547f477ca7fde0185f9cc57f',
-  appName: 'PDF Reader',
-  webDir: 'dist',
-  server: {
-    url: 'https://d5f4e1fd-547f-477c-a7fd-e0185f9cc57f.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
+  appId: 'com.mariswalegacy.app',
+  appName: 'Mari Swa Transcript',
+  webDir: 'dist'
 };
 
 export default config;
